29cea2dc0d7a02dbe0cc331556237a333a2a29c6
dchekhonin <feanor2626@gmail.com>
2015-11-10 17:45:32 +0200

    Create my_locators.py

---
+++ test_mail_classes/my_locators.py
@@ -0,0 +1,16 @@
+__author__ = 'dchekhonin'
+
+#mail_login class
+
+email_id = "Email"
+email_confirm_next_id = "next"
+email_password_id = "Passwd"
+email_signin_id = "signIn"
+
+#result_login_page
+
+to_be_sure_id = "gb"
+
+#mail_search_by_subject [xpath]
+
+list_of_all_mails = ".//*[@class='xY a4W']"
