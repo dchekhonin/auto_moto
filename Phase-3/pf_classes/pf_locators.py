d7eca1335c90d4c730e7aa2e963c13c93d795af9
dchekhonin <feanor2626@gmail.com>
2015-11-23 16:25:45 +0200

    Create pf_locators.py

---
+++ Phase-3/pf_classes/pf_locators.py
@@ -0,0 +1,24 @@
+__author__ = 'dchekhonin'
+
+
+#pf_login
+
+email_id = "Email"
+email_confirm_next_id = "next"
+email_password_id = "Passwd"
+email_signin_id = "signIn"
+email_error = "errormsg_0_Email"
+password_error = "errormsg_0_Passwd"
+
+#pf_result_homepage
+
+to_be_sure_id = "gb"
+
+#pf_new_mail
+
+sent_mail_to_be_sure_xpath = ".//*[@id='gs_taif50']"
+
+#pf_homepage
+
+sent_mail_menu = ".//*[@id=':5d']//a"
+inbox_mail_menu = ".//*[@id=':59']//a"
