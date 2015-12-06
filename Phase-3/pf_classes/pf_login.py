c1757aa5504ccf1079371213badff6d9b63f2a6a
dchekhonin <feanor2626@gmail.com>
2015-12-07 00:54:21 +0200

    Update pf_login.py

--- Phase-3/pf_classes/pf_login.py
+++ Phase-3/pf_classes/pf_login.py
@@ -5,12 +5,12 @@ from pf_homepage import HomePage
 import pf_locators
 from Page_factory.pageobject_support import cacheable, callable_find_by as find_by
 from selenium.webdriver.common.by import By
-from selenium  import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
+from Page_factory.pf_classes import LoggerMixin
+
 import time
 from datetime import datetime
 
-class LoginPage(object):
+class LoginPage(LoggerMixin):
 
     login_box = find_by(how=By.ID,using=pf_locators.email_id)
     submit_email_button = find_by(how=By.ID,using=pf_locators.email_confirm_next_id)
@@ -20,44 +20,33 @@ class LoginPage(object):
     password_error = find_by(how=By.ID,using=pf_locators.password_error)
 
     def __init__(self,driver):
+        super(LoginPage, self).__init__()
         self._driver = driver
 
 
     def login_error_mail(self,email):
-        try:
-            self.login_box().send_keys(email)
-            self.submit_email_button().click()
-            time.sleep(2)
-            return self.error_message().text
-        except Exception as e:
-            print e
-            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            self._driver.get_screenshot_as_file('screenshot-%s in LoginPage_login_er_mail.png' % now)
+        self.login_box().send_keys(email)
+        self.submit_email_button().click()
+        time.sleep(2)
+        return self.error_message().text
+
 
     def login_error_password(self,email,password):
-        try:
-            self.login_box().send_keys(email)
-            self.submit_email_button().click()
-            self.pass_box().send_keys(password)
-            self.submit_signin_button().click()
-            time.sleep(2)
-            return self.password_error().text
-        except Exception as e:
-            print e
-            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            self._driver.get_screenshot_as_file('screenshot-%s in LoginPage_login_er_pass.png' % now)
+        self.login_box().send_keys(email)
+        self.submit_email_button().click()
+        self.pass_box().send_keys(password)
+        self.submit_signin_button().click()
+        time.sleep(2)
+        return self.password_error().text
+
 
     def login(self,email,password):
-        try:
-            self.login_box().send_keys(email)
-            self.submit_email_button().click()
-            self.pass_box().send_keys(password)
-            self.submit_signin_button().click()
-            self._driver.implicitly_wait(5)
-            return HomePage(self._driver)
-        except Exception as e:
-            print e
-            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            self._driver.get_screenshot_as_file('screenshot-%s in LoginPage_login.png' % now)
+        self.login_box().send_keys(email)
+        self.submit_email_button().click()
+        self.pass_box().send_keys(password)
+        self.submit_signin_button().click()
+        self._driver.implicitly_wait(5)
+        return HomePage(self._driver)
+
 
 
