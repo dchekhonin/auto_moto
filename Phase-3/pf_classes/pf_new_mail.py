30c15769141eb92922fd504560b2db27c5858ae3
dchekhonin <feanor2626@gmail.com>
2015-12-07 00:55:05 +0200

    Update pf_new_mail.py

--- Phase-3/pf_classes/pf_new_mail.py
+++ Phase-3/pf_classes/pf_new_mail.py
@@ -1,15 +1,14 @@
-from datetime import datetime
+__author__ = 'dchekhonin'
 
-class Sent_mail_page(object):
+from Page_factory.pf_classes import LoggerMixin
+
+
+class Sent_mail_page(LoggerMixin):
 
 
     def __init__(self,driver):
+        super(Sent_mail_page, self).__init__()
         self._driver = driver
 
     def sent_to_be_sure_link(self):
-        try:
-            return self._driver.current_url
-        except Exception as e:
-            print e
-            now = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-            self._driver.get_screenshot_as_file('screenshot-%s in Sent_mail_sent_tbs_link.png' % now)
+        return self._driver.current_url
